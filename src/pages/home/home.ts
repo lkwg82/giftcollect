b10493aedcc904850d97ef5d64c6f022ada18fb9
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-07-25 23:17:18 +0200

    cleanup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,34 +1,20 @@
 import {Component} from '@angular/core';
 import {AuthServiceProvider} from "../../providers/auth-service/auth-service";
-import {AlertController} from "ionic-angular";
 import * as firebase from 'firebase/app';
-
-import {AboutPage} from "../about/about";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  aboutPage = AboutPage;
   username;
 
-  constructor(private _auth: AuthServiceProvider,
-              private alertCtrl: AlertController) {
+  constructor(private _auth: AuthServiceProvider) {
     let authState = this._auth.afAuth.authState;
     authState.subscribe((user: firebase.User) => {
       if (user) {
         this.username = user.displayName;
       }
     });
-  }
-
-  presentAlert() {
-    let alert = this.alertCtrl.create({
-      title: 'Low battery',
-      subTitle: '10% of battery remaining',
-      buttons: ['Dismiss']
-    });
-    alert.present();
   }
 }
