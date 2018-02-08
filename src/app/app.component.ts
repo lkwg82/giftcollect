9680c056de2454c5679af6df18508c4ac781e40c
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-02-08 09:33:15 +0100

    cleanup: removed warnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,10 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+
+      // TODO
+      // statusBar.styleDefault();
+      // splashScreen.hide();
     });
   }
 
