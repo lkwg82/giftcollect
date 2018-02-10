5e8a0f7fe004017706d10058379871355defee63
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-02-10 22:58:54 +0100

    adds binding of an object

---
+++ src/app/domain/gift.ts
@@ -0,0 +1,11 @@
+export class Gift {
+  public title: String = "";
+  public description: String = "";
+  public estimatedPrice: number = 0;
+  public doNotExceedPrice: boolean = false;
+  public doAskBeforeBuy: boolean = false;
+
+  constructor() {
+  }
+
+}
