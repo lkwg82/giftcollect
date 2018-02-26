ef33741c2855f0b242e4e60764ff370629d24095
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-02-26 14:48:34 +0100

    cleanup

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,36 +3,41 @@ import {GiftStore} from "../../providers/giftstore";
 import {NavController} from "ionic-angular";
 import {GiftPage} from "../gift/gift";
 import {Gift} from "../../app/domain/gift";
-import {AuthServiceProvider} from "../../providers/auth-service/auth-service";
+import {Subject} from "rxjs/Subject";
 
 @Component({
              selector: 'page-home',
              templateUrl: 'home.html'
            })
 export class HomePage {
+  private stop$: Subject<void> = new Subject<void>();
+
   gifts: Gift[] = [];
 
   constructor(public giftStore: GiftStore,
-              private _navCtr: NavController,
-              private _auth: AuthServiceProvider) {
+              private _navCtrl: NavController) {
   }
 
   ionViewDidLoad() {
     console.log("homepage loaded");
     this.giftStore
         .valueChanges()
-        .takeUntil(this._auth.signedOut)
+        .takeUntil(this.stop$)
         .subscribe((gifts: Gift[]) => {
           gifts.sort((a, b) => a.title.localeCompare(b.title));
           this.gifts = gifts
         });
   }
 
-  addGift() {
-    this._navCtr.push(GiftPage);
+  ionViewDidLeave() {
+    this.stop$.complete();
   }
 
-  itemSelected(gift: Gift) {
-    this._navCtr.push(GiftPage, {"gift": gift})
+  addGift() {
+    this._navCtrl.push(GiftPage);
+  }
+
+  edit(gift: Gift) {
+    this._navCtrl.push(GiftPage, {"gift": gift})
   }
 }
